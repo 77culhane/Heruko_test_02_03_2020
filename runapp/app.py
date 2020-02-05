ef0bff7ae6ed7dc13f4808ce2ff4d444d31cb26b
John <77culhane@cua.edu>
2020-02-05 13:48:58 -0500

    push

--- runapp/app.py
+++ runapp/app.py
@@ -20,19 +20,11 @@ import pymysql
 # Pandas
 import pandas as pd
 
-# Import your config
-if is_heroku == False:
-    remote_db_endpoint = ''
-    remote_db_port = 0
-    remote_gwsis_dbname = ''
-    remote_gwsis_dbuser = ''
-    remote_gwsis_dbpwd = ''
-else:
-    remote_db_endpoint = os.environ.get('remote_db_endpoint')
-    remote_db_port = os.environ.get('remote_db_port')
-    remote_gwsis_dbname = os.environ.get('remote_gwsis_dbname')
-    remote_gwsis_dbuser = os.environ.get('remote_gwsis_dbuser')
-    remote_gwsis_dbpwd = os.environ.get('remote_gwsis_dbpwd')
+remote_db_endpoint = os.environ.get('remote_db_endpoint')
+remote_db_port = os.environ.get('remote_db_port')
+remote_gwsis_dbname = os.environ.get('remote_gwsis_dbname')
+remote_gwsis_dbuser = os.environ.get('remote_gwsis_dbuser')
+remote_gwsis_dbpwd = os.environ.get('remote_gwsis_dbpwd')
     
 # Configure MySQL connection and connect 
 pymysql.install_as_MySQLdb()
