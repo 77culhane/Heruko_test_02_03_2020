db1b107ccf3478312e1293161566805ddf2390f4
John <77culhane@cua.edu>
2020-02-06 13:50:28 -0500

    final

--- runapp/app.py
+++ runapp/app.py
@@ -22,11 +22,11 @@ import pandas as pd
 
 # Import your config
 if is_heroku == False:
-    remote_db_endpoint = 'codingbootcamp.cnm4q1bfp1uz.us-east-2.rds.amazonaws.com'
-    remote_db_port = 3306
-    remote_gwsis_dbname = 'GW_SIS'
-    remote_gwsis_dbuser = 'admin'
-    remote_gwsis_dbpwd = 'newpassword'
+    remote_db_endpoint = ''
+    remote_db_port = 0
+    remote_gwsis_dbname = ''
+    remote_gwsis_dbuser = ''
+    remote_gwsis_dbpwd = ''
 else:
     remote_db_endpoint = os.environ.get('remote_db_endpoint')
     remote_db_port = os.environ.get('remote_db_port')
